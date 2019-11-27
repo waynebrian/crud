4fd854b261c003dc2d1ab59f45eaf586f763cca7
unknown <onilongo.johnwayne@gmail.com>
2019-11-27 17:47:06 +0800

    Startup files

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Form</title>
+</head>
+<body>
+
+		
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,8 @@
+<?php include('index.php'); ?>
+
+<div class="container">
+			<div class="block">
+				<h1 class="title">Login Form
+				</h1>
+			</div>
+		</div>
