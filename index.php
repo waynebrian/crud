8925b6e82a338f43580ec1171ce97a8a700ef17d
emelen123 <emelenlopez3@gmail.com>
2019-11-29 18:34:27 +0800

    crud-emelen-logincss

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <html>
 <head>
 	<title>Login Form</title>
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
+	<script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 </head>
 <body>
 
--- login.php
+++ login.php
@@ -4,11 +4,33 @@
 			<div class="block">
 				<h1 class="title">Login Form
 				</h1>
-				<span class="content">
-					<a href="#">Forgot Password</a>
-				</span>
+					<div class="field">
+			  <p class="control has-icons-left has-icons-right">
+			    <input class="input is-medium" type="email" placeholder="Email" required>
+			    <span class="icon is-small is-left">
+			      <i class="far fa-user"></i>
+			    </span>
+			    <span class="icon is-small is-right">
+			      <i class="fas fa-check"></i>
+			    </span>
+			  </p>
 			</div>
-			<div class="button">Button</div>
-			<div class="btn">button</div>
+			<div class="field">
+			  <p class="control has-icons-left">
+			    <input class="input is-medium" type="password" placeholder="Password" required>
+			    <span class="icon is-small is-left">
+			      <i class="far fa-eye"></i>
+			    </span>
+			  </p>
+			</div>
+			<div class="field">
+			  <p class="control">
+			    <button class="button is-success is-medium">
+			      Login
+			    </button>
+			  </p>
+			</div>
+			</div>
+		
 		</div>
 
